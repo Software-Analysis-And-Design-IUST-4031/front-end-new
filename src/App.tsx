50a77f06eb395700c3a7a4d140b9601e19af6e6d
rrrr-del <rrrraaaaaaa79@gmail.com>
2024-11-16 09:28:31 +0330

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './SignUp'
 import Login from './Login'
+import Galleries from './components__galleries/galleries';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Login />}/>
         <Route path='/Login' element={<Login />}/>
         <Route path='/SignUp' element={<SignUp />}/>
+        <Route path='/galleries' element={<Galleries />}/>
       </Routes>
     </Router>
   );
