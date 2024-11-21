31d6426993d5912723a2109bc62501aba02a131d
Aminjanani <aminjanani13@gmail.com>
2024-11-22 03:05:49 +0330

    add : is_gallery combo box to edit profile subtab

--- src/ProfileEditor.tsx
+++ src/ProfileEditor.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { Drawer, ScrollArea, TextInput, Stack, Button, PasswordInput, Box, Grid, MantineProvider, Tabs} from '@mantine/core'
+import { Drawer, ScrollArea, TextInput, Select, Popover, List, ListItem, Button, Radio, PasswordInput, Box, Grid, MantineProvider, Tabs} from '@mantine/core'
 //import { Navigate, useNavigate } from "react-router-dom";
 import { EyeCheck, EyeOff, GitFork } from 'tabler-icons-react';
 
@@ -12,8 +12,19 @@ const ProfileEditor = () => {
   const [phoneNumber, setPhoneNumber] = useState('');
   const [country, setCountry] = useState('');
   const [city, setCity] = useState('');
+  const [dateOfBirth, setDateOfBirth] = useState('');
+  const [isGallery, setIsGallery] = useState<string>('NO');
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+  const [opened, setOpened] = useState(false);
   const [activeTab, setActiveTab] = useState<string | null>('profile-editor');
 
+  const [favoritePainter, setFavoritePainter] = useState('');
+
+  const handeOptionsSelect = (value: string | null) => {
+    setSelectedValue(value);
+    setOpened(false);
+  }
+ 
   const handleFirstnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setFirstName(value);
@@ -54,6 +65,16 @@ const ProfileEditor = () => {
     setCity(value);
   }
 
+  const handleDateOfBirthChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setDateOfBirth(value);
+  }
+
+  const handleFavoritePainterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setFavoritePainter(value);
+  }
+
   return (
     <MantineProvider>
       <Box
@@ -67,7 +88,7 @@ const ProfileEditor = () => {
           margin: '0 auto',
           marginBottom: '170px',
           padding: '20px 20px 20px 20px',
-          paddingTop: '5px',
+          paddingTop: '3px',
           paddingLeft: '15px',
           paddingRight: '15px',
           border: '2px solid rgba(0, 0, 0, 0.158)',
@@ -84,7 +105,7 @@ const ProfileEditor = () => {
             },
             tab: {
               flex: 1,
-              borderRadius: '5px',
+              borderRadius: '0px',
               textAlign: 'center',
               color: 'white',
               backgroundColor: 'black',
@@ -101,7 +122,7 @@ const ProfileEditor = () => {
         >
           <Tabs.List>
             <Tabs.Tab value="profile-editor">ProfileEditor</Tabs.Tab>
-            <Tabs.Tab value="favoites">favorites</Tabs.Tab>
+            <Tabs.Tab value="favorites">favorites</Tabs.Tab>
           </Tabs.List>
           <Tabs.Panel value="profile-editor" pt="lg">
             <Grid>
@@ -261,7 +282,7 @@ const ProfileEditor = () => {
             <Grid.Col span={6}>
             <label>
               <TextInput
-                label='Country'
+                label='Country/Nationality'
                 type='text'
                 name="country"
                 value={country}
@@ -311,9 +332,157 @@ const ProfileEditor = () => {
               </label>
             </Grid.Col>
             </Grid>
+            <Grid>
+              <Grid.Col span={6}>
+              <label>
+                <TextInput
+                  label='Date of Birth'
+                  type='date'
+                  name="dateofbirth"
+                  value={dateOfBirth}
+                  onChange={handleDateOfBirthChange}
+                  placeholder="enter your city"
+                  labelProps={{ className:'label-aligned'}}
+                  styles={{
+                    input: {
+                      width: '230px',
+                      borderRadius: '7px'
+                    },
+                    label: {
+                      fontSize: '15px',
+                      fontWeight: '550',
+                      marginBottom: '5px',
+                      textAlign: "left",
+                      marginLeft: '0px'
+                    }
+                  }} 
+                />
+                </label>
+              </Grid.Col>
+              <Grid.Col span={6}>
+              <label>
+                <Select
+                  label="Are you a gallery?"
+                  placeholder="select YES or NO"
+                  data={["YES", "NO"]}
+                  value={selectedValue}
+                  onChange={handeOptionsSelect}
+                  styles={{
+                    input: {
+                      width: '230px',
+                      borderRadius: '7px'
+                    },
+                    label: {
+                      fontSize: '15px',
+                      fontWeight: '550',
+                      marginBottom: '5px',
+                      textAlign: "left",
+                      marginLeft: '0px'
+                  }
+                }}
+              />
+              </label>
+            </Grid.Col>
+            </Grid>
+            <Grid>
+            <Grid.Col>
+            <Button
+                type='submit'
+                variant='contained'
+                fullWidth
+                style={{
+                  color: 'white',
+                  backgroundColor: 'black',
+                  '&:hover': {
+                    backgroundColor: 'white',
+                    color: 'black'
+                  },
+                  mt: 0.25,
+                  py: 0.7,
+                  borderRadius: '7px'
+                }}
+              > 
+                submit
+              </Button>
+            </Grid.Col>
+          </Grid>
           </Tabs.Panel>
           <Tabs.Panel value="favorites" pt="lg">
-            First
+          <Grid>
+              <Grid.Col span={6}>
+                <label>
+                  <TextInput
+                    label='Who is your favorite painter?'
+                    type='text'
+                    name="favorite_painter"
+                    value={favoritePainter}
+                    onChange={handleFavoritePainterChange}
+                    placeholder="favorite painter"
+                    labelProps={{ className:'label-aligned'}}
+                    styles={{
+                      input: {
+                        width: '230px',
+                        borderRadius: '7px'
+                      },
+                      label: {
+                        fontSize: '15px',
+                        fontWeight: '550',
+                        marginBottom: '5px',
+                        textAlign: "left",
+                        marginLeft: '0px'
+                      }
+                    }} 
+                  />
+                  </label>
+                </Grid.Col>
+                <Grid.Col span={6}>
+              <label>
+                <TextInput 
+                  label='Lastname'
+                  type='text'
+                  name="last_name"
+                  value={lastName}
+                  onChange={handleLastnameChange}
+                  placeholder='enter your new last name'
+                  labelProps={{ className:'label-aligned'}}
+                  styles={{
+                    input: {
+                      width: '230px',
+                      borderRadius: '7px'
+                    },
+                    label: {
+                      fontSize: '15px',
+                      fontWeight: '550',
+                      marginBottom: '5px',
+                      textAlign: "left",
+                      marginLeft: '0px'
+                    }}}
+                />
+              </label>
+            </Grid.Col>
+            </Grid>
+            <Grid justify='center'>
+            <Grid.Col>
+            <Button
+                type='submit'
+                variant='contained'
+                fullWidth
+                style={{
+                  color: 'white',
+                  backgroundColor: 'black',
+                  '&:hover': {
+                    backgroundColor: 'white',
+                    color: 'black'
+                  },
+                  mt: 0.25,
+                  py: 0.7,
+                  borderRadius: '7px'
+                }}
+              > 
+                submit
+              </Button>
+            </Grid.Col>
+          </Grid>
           </Tabs.Panel>
         </Tabs>
       </Box>
