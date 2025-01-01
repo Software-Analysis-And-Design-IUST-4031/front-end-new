fd35886f44616189e4406a40a7ff6b1a5a293bff
Rickasikhan <kasrakhalaj44@gmail.com>
2025-01-01 22:17:29 +0330

    successfully handled deployed errors in blog and added Maximize minimize feature in painting panel paining image pop up

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -5,12 +5,10 @@ export interface Blog {
   title: string;
   content: string;
   created_at: string;
-  updated_at: string;
-  author: {
-    id: number;
-    username: string;
-  };
+  updated_at?: string;
+  author_name: string;
   image?: string;
+  comments?: Comment[];
 }
 
 export interface Comment {
@@ -36,8 +34,20 @@ const blogService = {
   },
 
   getBlog: async (id: number): Promise<Blog> => {
-    const response = await api.get<Blog>(`/blogs/blogs/${id}/`);
-    return response.data;
+    // Get all blogs and find the one we want
+    const response = await api.get<Blog[]>('/blogs/blogs/');
+    const blog = response.data.find(b => b.id === id);
+    
+    if (!blog) {
+      throw new Error('Blog not found');
+    }
+
+    if (!blog.author_name) {
+      console.error('Blog data is missing author name:', blog);
+      throw new Error('Invalid blog data structure');
+    }
+
+    return blog;
   },
 
   createBlog: async (formData: FormData): Promise<Blog> => {
@@ -50,7 +60,11 @@ const blogService = {
   },
 
   updateBlog: async (id: number, formData: FormData): Promise<Blog> => {
-    const response = await api.put<Blog>(`/blogs/blogs/${id}/`, formData, {
+    // Add the ID to identify which blog to update
+    formData.append('blog_id', id.toString());
+    
+    // Use PUT method to indicate this is an update
+    const response = await api.put<Blog>('/blogs/blogs/', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
@@ -59,12 +73,16 @@ const blogService = {
   },
 
   deleteBlog: async (id: number): Promise<void> => {
-    await api.delete(`/blogs/blogs/${id}/`);
+    // Send delete request with the ID in the body
+    await api.post('/blogs/blogs/', {
+      blog_id: id,
+      action: 'delete'
+    });
   },
 
   getComments: async (blogId: number): Promise<Comment[]> => {
-    const response = await api.get<Comment[]>(`/blogs/blogs/${blogId}/comments/`);
-    return response.data;
+    const blog = await blogService.getBlog(blogId);
+    return blog.comments || [];
   },
 
   addComment: async (blogId: number, commentData: CommentCreateData): Promise<Comment> => {
@@ -73,7 +91,10 @@ const blogService = {
   },
 
   deleteComment: async (blogId: number, commentId: number): Promise<void> => {
-    await api.delete(`/blogs/blogs/${blogId}/comments/${commentId}/`);
+    await api.post(`/blogs/blogs/${blogId}/comments/`, {
+      comment_id: commentId,
+      action: 'delete'
+    });
   }
 };
 
